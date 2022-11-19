dbb0a45003b0e3d1dc05566eb002e59c2902a83f
Ksyusha <sun_wenz03@mail.ru>
2022-11-19 20:54:12 +0300

    init

---
+++ src/main/java/ru/skydiver/backend/skydiver/SkydiverApplication.java
@@ -0,0 +1,13 @@
+package ru.skydiver.backend.skydiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkydiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkydiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skydiver/backend/skydiver/SkydiverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skydiver.backend.skydiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkydiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
